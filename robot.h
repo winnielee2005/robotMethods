20617e4cf5cc0caf98168f49ff8888792e86eec4
winnielee0401 <winnielee0401@gmail.com>
2025-05-23 18:28:35 +0800

    created base class "Robot" and derived class "SeeingRobot"

--- robot.h
+++ robot.h
@@ -1,3 +1,44 @@
 // Header file just declares what everything does, real fucntions go into cpp file
 #ifndef ROBOT_H
 #define ROBOT_H
+
+#include <string>
+#include <vector>
+
+
+class Robot {
+    protected:
+    int x, y; 
+    int lives = 3;
+    std::string name;
+    std::string type;
+
+    public:
+    //Constructor to give new robot a name, starting x point, starting y point
+    Robot(const std::string name, std::string type, int x, int y); 
+
+    virtual ~Robot()
+    {
+         // does nothing for now
+    }
+    
+    // nothing here yet, just a rule
+    virtual void move() = 0;
+    virtual void look() = 0;
+    virtual void fire() = 0;
+    virtual void think() = 0;
+    
+    // getter functions for robot name, position x, position y and whether is alive or not
+    std::string getName();
+    int getX();
+    int getY(); 
+    bool isAlive();
+};
+
+class SeeingRobot : virtual public Robot {
+    public:
+    SeeingRobot(std::string name, std::string type, int x, int y);
+    virtual void look(int x, int y) override = 0;
+};
+
+#endif
